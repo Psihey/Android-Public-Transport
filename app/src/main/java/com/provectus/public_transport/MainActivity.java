17abad0e28d83e17d8f840dcd8a4c02be803c166
Psihey <psihey1@gmail.com>
2017-08-02 23:33:46 +0300

    Create Android project

---
+++ app/src/main/java/com/provectus/public_transport/MainActivity.java
@@ -0,0 +1,13 @@
+package com.provectus.public_transport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
