d23f8820e7f2828c0988d233f32a5da52637e83e
Psihey <psihey1@gmail.com>
2017-08-11 11:31:10 +0300

    Add Fabric to the project

--- app/src/main/java/com/provectus/public_transport/MainActivity.java
+++ app/src/main/java/com/provectus/public_transport/MainActivity.java
@@ -2,6 +2,8 @@ package com.provectus.public_transport;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.container,
