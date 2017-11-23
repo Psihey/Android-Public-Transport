065290520fb392d271059ea6e24faa5ea42090ee
Psihey <psihey1@gmail.com>
2017-11-23 15:53:45 +0200

    Fixed strings

--- app/src/main/java/com/provectus/public_transport/PublicTransportApplication.java
+++ app/src/main/java/com/provectus/public_transport/PublicTransportApplication.java
@@ -1,7 +1,6 @@
 package com.provectus.public_transport;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.crashlytics.android.Crashlytics;
 import com.orhanobut.logger.AndroidLogAdapter;
@@ -12,19 +11,12 @@ import io.fabric.sdk.android.Fabric;
 
 public class PublicTransportApplication extends Application {
 
-    static private Context sContext;
-
     @Override
     public void onCreate() {
         super.onCreate();
         Fabric.with(this, new Crashlytics());
         DatabaseHelper.createPublicTransportDatabase(getApplicationContext());
-        sContext = getApplicationContext();
         Logger.addLogAdapter(new AndroidLogAdapter());
-    }
-
-    public static Context getContext(){
-        return sContext;
     }
 
 }
